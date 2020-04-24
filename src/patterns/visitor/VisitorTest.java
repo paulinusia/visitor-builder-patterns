b866836837270f4f4e0fa63eb15f0bdeabae4c46
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-04-24 17:55:04 -0500

    added test file

---
+++ src/patterns/visitor/VisitorTest.java
@@ -0,0 +1,14 @@
+package patterns.visitor;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class VisitorTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
