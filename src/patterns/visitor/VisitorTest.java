c6d2f42cfe02817650233f1d769d7290298042b9
alexandratcrane1999 <alexandratcrane@gmail.com>
2020-04-25 11:27:34 -0500

    creating tests. added a way to choose the tax rate for the items

--- src/patterns/visitor/VisitorTest.java
+++ src/patterns/visitor/VisitorTest.java
@@ -3,12 +3,48 @@ package patterns.visitor;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
 
 public class VisitorTest {
-
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testCost() {
+		Fruit plum = new Fruit(2.00);
+		Meat steak = new Meat(25.00);
+		Toiletries shampoo = new Toiletries(10.00);
+		
+		assertEquals(2.00, plum.getCost(), 0.01);
+		assertEquals(25.00, steak.getCost(), 0.01);
+		assertEquals(10.00, shampoo.getCost(), 0.01);
+		
+	}
+	
+
+
+	public void testCostTax() {
+		TaxVisitor taxation = new TaxVisitor();
+		Fruit plum = new Fruit(2.00);
+		Meat steak = new Meat(25.00);
+		Toiletries shampoo = new Toiletries(10.00);
+		
+		taxation.setTaxAll(1.09);
+		plum.accept(taxation);
+		steak.accept(taxation);
+		shampoo.accept(taxation);
+		
+		assertEquals(2.18, plum.getCost(), 0.01);
+		assertEquals(27.25, steak.getCost(), 0.01);
+		assertEquals(10.90, shampoo.getCost(), 0.01);
+		
+		
+		
+	}
+	
+	public void testCostAll() {
+		
+		
 	}
 
 }
